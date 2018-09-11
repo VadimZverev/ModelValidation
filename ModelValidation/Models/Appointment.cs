c97713fbaf25c3781c861886abdcdc50a2f2067d
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-09-11 14:59:11 +0400

    Определяем модели с автоматической валидацией

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -1,21 +1,45 @@
 ﻿using ModelValidation.Infrastructure;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidation.Models
 {
     [NoJoeOnMondays]
-    public class Appointment
+    public class Appointment : IValidatableObject
     {
-        [Required]
+        //[Required]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
-        [FutureDate(ErrorMessage = "Please enter a date in the future")]
+        //[FutureDate(ErrorMessage = "Please enter a date in the future")]
         public DateTime Date { get; set; }
 
         //[Range(typeof(bool),"true","true", ErrorMessage = "You must accept the terms")]
-        [MustBeTrue(ErrorMessage = "You must accept the terms")]
+        //[MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+            if (string.IsNullOrEmpty(ClientName))
+            {
+                errors.Add(new ValidationResult("Please enter your name"));
+            }
+            if (DateTime.Now > Date)
+            {
+                errors.Add(new ValidationResult("Please enter date in future"));
+            }
+            if (errors.Count == 0 && ClientName == "Joe"
+                && Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                errors.Add(new ValidationResult("Joe cannot book appointments on Mondays"));
+            }
+            if (!TermsAccepted)
+            {
+                errors.Add(new ValidationResult("You must accept the terms"));
+            }
+            return errors;
+        }
     }
 }
