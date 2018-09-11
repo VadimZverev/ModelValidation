ec9023866b43266a3baa318bd125ba8cc3a635aa
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-09-11 10:58:59 +0400

    Подготовка проекта для главы

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ModelValidation.Models;
+using System;
+using System.Web.Mvc;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            // инструкции для хранения нового Appointment
+            // в репозитории будут идти здесь в реальном проекте
+
+            return View("Completed", appt);
+        }
+    }
+}
